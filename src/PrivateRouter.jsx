64a0d99e0da74015c15bf8c64d367c983b1613ef
Zerotohero1234 <anouwatt@gmail.com>
2022-08-08 01:49:05 +0700

    new

--- src/PrivateRouter.jsx
+++ src/PrivateRouter.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Route,Outlet, Navigate } from "react-router-dom"
-import { useSelector } from "react-redux"
+import React from "react";
+import { Route, Outlet, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 function PrivateRouter() {
   // const userLogin = useSelector((state) => state.userLogin);
   // const { userInfo } = userLogin;
-  const token = window.localStorage.getItem("userInfo")
-  return token ? <Outlet /> : <Navigate to="/login" />
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  return userInfo ? <Outlet /> : <Navigate to="/login" />;
 }
 
-export default PrivateRouter
+export default PrivateRouter;
