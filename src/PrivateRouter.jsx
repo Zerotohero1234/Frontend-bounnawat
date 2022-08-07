4100883dcfc38865e5ff93900cfc2ca8955aa902
Zerotohero1234 <anouwatt@gmail.com>
2022-08-08 01:55:56 +0700

    userinfo

--- src/PrivateRouter.jsx
+++ src/PrivateRouter.jsx
@@ -4,8 +4,9 @@ import { useSelector } from "react-redux";
 function PrivateRouter() {
   // const userLogin = useSelector((state) => state.userLogin);
   // const { userInfo } = userLogin;
-  const token = window.localStorage.getItem("userInfo");
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  return userInfo ? <Outlet /> : <Navigate to="/login" />;
 }
 
 export default PrivateRouter;
